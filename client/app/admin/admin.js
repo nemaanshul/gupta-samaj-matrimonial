6b01e9cca8c306d8d8075f098f328aad1d60ed3d
anshul.a.nema <anshul.a.nema@accenture.com>
2016-05-17 23:42:08 +0530

    new repo created

---
+++ client/app/admin/admin.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('es6App')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/admin', {
+        templateUrl: 'app/admin/admin.html',
+        controller: 'AdminCtrl'
+      });
+  });
